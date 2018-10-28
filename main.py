04ba8bfb2d4d091d241caeebd07e98f89fb913cd
Milan Terhes <terhes.milan@gmail.com>
2018-10-28 21:10:49 +0100

    added database

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+import sqlite3
 
 class HookHandler(BaseHTTPRequestHandler):
     def challengeResponse(self, challenge):
@@ -10,32 +11,49 @@ class HookHandler(BaseHTTPRequestHandler):
         print(challenge)
         self.wfile.write(challenge)
 
-    def verify_lead(self, msg):
-        text = msg["event"]["text"]
-        if "to" in text or "from" in text or "email" in text:
-            print(json.loads(text))
+    def handle_event(self, msg):
+        if msg["event"]["channel"] == "CDQPMC7PY":
+
+            text = msg["event"]["text"] #message sent by the user
+            date = {"date": msg["event_time"]}
+            text.update(date)
+
+            if "to" in text or "from" in text or "email" in text:
+                print(text)
+
+                #save 
+                conn = sqlite3.connect('example.db')
+                c = conn.cursor()
+                c.execute("""CREATE TABLE if not exists flights(
+                                start TEXT,
+                                destination TEXT NOT NULL,
+                                email TEXT NOT NULL,
+                                date integer NOT NULL
+                                );""")
+                new_record = (text["from"], text["to"], text["email"], text["date"])
+                c.execute("INSERT INTO flights VALUES(?,?,?,?);",new_record)
+                conn.commit()
+                c.close()
 
     def do_POST(self):
+        #get the body of the request
         request_length = int(self.headers.get('Content-Length'))
         raw_req = self.rfile.read(request_length).decode('utf-8')
         message = json.loads(raw_req)
         
+        #this had to be ran only once for slack to authenticate
         #self.challengeResponse(bytes(request_json['challenge'], 'utf-8'))
+
+        #200 response needed for slack otherwise it sends the event multiple times
         self.send_response(200)
         self.end_headers()
-        #print('received event: ', request_json["event"]["text"], request_json["event_time"])
-        
-        if message["event"]["channel"] == "CDQPMC7PY":
-            self.verify_lead(message)
 
-
-
+        self.handle_event(message)
 
 def run(server_class=HTTPServer, handler_class=HookHandler):
-    server_adress = ('', 80)
+    server_adress = ('', 8080)
     httpd = server_class(server_adress, handler_class)
     print("launching server...")
     httpd.serve_forever()
 
 run()
-
